b37a9ac5baccaf0f170ace5c3df6d2a8db0350a6
dreacot <kennedyizuegbu@yahoo.com>
2023-09-28 14:52:20 -0600

    initial commit

---
+++ app/src/main/java/com/dreacotgroup/cryptopower/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.dreacotgroup.cryptopower
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.gioui.Gio
+import org.gioui.GioActivity
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 101
+        private const val OPEN_DIRECTORY_REQUEST_CODE = 102
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Gio.init(this)
+
+        // If you are targeting Android 11 and above, use this condition
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+            startActivityForResult(intent, OPEN_DIRECTORY_REQUEST_CODE)
+        } else {
+            // For Android 10 and below
+            checkPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                STORAGE_PERMISSION_CODE
+            )
+        }
+
+        setContentView(R.layout.activity_main)
+//        startActivity(Intent(this, GioActivity::class.java))
+    }
+
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+//            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, GioActivity::class.java))
+
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, GioActivity::class.java))
+
+            } else {
+                Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}

d09aac895138bfa9af72d673221f6313c170a1b2
dreacot <kennedyizuegbu@yahoo.com>
2023-09-28 15:12:12 -0600

    initial commit

--- app/src/main/java/com/dreacotgroup/cryptopower/MainActivity.kt
+++ app/src/main/java/com/dreacotgroup/cryptopower/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
-//        startActivity(Intent(this, GioActivity::class.java))
+        startActivity(Intent(this, GioActivity::class.java))
     }
 
     // Function to check and request permission.
